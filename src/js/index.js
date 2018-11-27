1cbd744662fbec6bd9f800e1fc6af4618f09e58a
Alicia Geijer <0002alge@res.ludvika.intra>
2018-11-27 15:49:32 +0100

    gjort klart utstyrsel

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+
+// TODO: börja med att kolla Websocket under uppgifter och läs vilka meddelanden man kan pröva för att hämta å skicka data.
