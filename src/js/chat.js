c6c15be04ae473656b66437c6ab56d9b75908e25
Alicia Geijer <0002alge@res.ludvika.intra>
2018-11-27 15:13:37 +0100

    klart utseende för chatt

---
+++ src/js/chat.js
@@ -0,0 +1,30 @@
+// TODO: En fungerande chat över en WebSocket uppkoppling som
+//       tillåter dig att skicka och ta emot meddelanden
+
+// TODO: Kunna välja ett användarnamn.
+
+// TODO: Strukturerade med HTML
+
+// TODO: Stilsatt med CSS
+
+// TODO: Interaktiv med JavaScript
+
+// TODO: Kunna skicka meddelanden både genom att trycka på en
+//       skicka knapp och genom att trycka på [ENTER]
+
+// TODO: När man startar chatten läsa in tidigare chatt historik.
+
+// TODO: Tilldela varje användare en färg.
+
+// TODO: Man ska tydligt kunna se vem som har skickat ett meddelande.
+
+// TODO: Skriv ut tid för när ett meddelande skickades.
+
+// TODO: En enklare dokumentation skriven i README.md i markup språket
+//       Markdown som talar om vad som ligger i vilka filer och vilka
+//       kommandon som kan vara värda att känna till.
+
+// TODO: Regelbundna commits
+
+// TODO: Chattens utseende ska se någorlunda bra ut. (Bör se ut som
+//       en chatt och inte som något annat)
